437b55da514584adb21d3246f28e17451c2714ba
mlollino <mlollino@purdue.edu>
2024-04-10 15:24:22 -0400

    ML

--- MiniProjectPath1.py
+++ MiniProjectPath1.py
@@ -1,4 +1,6 @@
 import pandas
+import numpy as np
+
 '''
  The following is the starting code for path1 for data reading to make your first step easier.
  'dataset_1' is the clean data for path1.
@@ -19,16 +21,41 @@ df['numRWs']      = pandas.to_numeric(df['numRWs'])
 df['numFFs']      = pandas.to_numeric(df['numFFs'])
 df['s']           = pandas.to_numeric(df['s'])
 dataset_1 = df
-print(dataset_1[15620:25350].to_string()) #This line will print out the first 35 rows of your data
+#print(dataset_1[15620:25350].to_string()) #This line will print out the first 35 rows of your data
 
-#clean data
+print('Length of data set before 5 video filter: ', len(dataset_1))
 
+#Initializes lists and dictionary
+dataset_1 = dataset_1.values.tolist()
 clean_data = []
 new_users = []
+id_counts = {}
 
-for i in range(len(dataset_1)):
-    if dataset_1[i][0] in new_users:
-        clean_data.append(dataset_1[i])
+#counts how many times each ID shows up in dataset_1, assigns count to ID in dictionary
+for row in dataset_1:
+    user_id = row[0]
+    if user_id in id_counts:
+        id_counts[user_id] += 1
+    else: 
+        id_counts[user_id] = 1
+
+#makes new list of user IDs only if user_ID has five or more counts in dictionary
+for user_id, count in id_counts.items():
+    if count >= 5:
+        new_users.append(user_id)
+
+#makes new list of lists based off of filtered User IDs
+for row in dataset_1:
+    if row[0] in new_users:
+        clean_data.append(row)
+
+print('Length of data set after 5 video filter: ', len(clean_data))
+
+#converts to array
+X = np.array(clean_data)
+
+#Example of filtered data (may be a problem in the future that values are strings and not floats/integers)
+print('First two rows of cleaned data: ', X[:2])
 
 
 
