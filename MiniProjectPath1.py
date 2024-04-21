b06a9e05c498cb54f13ca774fb1f195194ca0408
mlollino <mlollino@purdue.edu>
2024-04-21 19:06:34 -0400

    V2

--- MiniProjectPath1.py
+++ MiniProjectPath1.py
@@ -1,5 +1,12 @@
+import matplotlib.pyplot as plt
 import pandas
 import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import Ridge
 
 '''
  The following is the starting code for path1 for data reading to make your first step easier.
@@ -35,81 +42,68 @@ dataset_1_filtered = video_completion[video_completion['userID'].isin(users_with
 
 dataset_1_filteredArray = dataset_1_filtered.to_numpy() #Final Filtered array
 
+print('Shape of Original Dataset: ' ,dataset_1.shape)
+print('Shape of Filtered Dataset: ' ,dataset_1_filteredArray.shape)
+
+Desired_Features = dataset_1_filtered[['fracSpent', 'fracComp', 'fracPaused', 'numPauses', 'avgPBR', 'numRWs', 'numFFs']]
+X = np.array(Desired_Features)
+print('Shape of Filtered Dataset with Desired Features: ', X.shape )
+
+####################################################################KMEANS##############################################
+kmeans = KMeans(n_clusters=5, random_state = 0)
+kmeans.fit(X)
+print(kmeans.cluster_centers_)
+print(kmeans.labels_)
+
+dataset_1_filtered['Cluster'] = kmeans.labels_
+
+# Run PCA and reduce the data to two components for visualization
+pca = PCA(n_components=2)
+features_reduced = pca.fit_transform(X)
 
 
+plt.scatter(features_reduced[:, 0], features_reduced[:, 1], c=dataset_1_filtered['Cluster'])
+plt.xlabel('PCA Component 1')
+plt.ylabel('PCA Component 2')
+plt.show()
 
+# Visualize Clusters for Desired Two Features
+dataset_1_filtered['Cluster'] = kmeans.labels_
 
+x = 'fracSpent'
+y = 'fracPaused'
 
+plt.scatter(Desired_Features[x], Desired_Features[y], c=kmeans.labels_)
+plt.title('Clusters of Student Video-Watching Behaviors')
+plt.xlabel(x)
+plt.ylabel(y)
+plt.show()
 
-print('Length of data set before 5 video filter: ', len(dataset_1))
+##################################################LinearRegression############################################3
 
-#Initializes lists and dictionary
-dataset_1 = dataset_1.values.tolist()
-clean_data = []
-new_users = []
-id_counts = {}
+X = dataset_1_filtered[['fracSpent','fracComp','fracPlayed','fracPaused','numPauses','avgPBR','stdPBR','numRWs','numFFs']]
+y = dataset_1_filtered[['s']]
 
-#counts how many times each ID shows up in dataset_1, assigns count to ID in dictionary
-for row in dataset_1:
-    user_id = row[0]
-    if user_id in id_counts:
-        id_counts[user_id] += 1
-    else: 
-        id_counts[user_id] = 1
+X_train, X_test, y_train, y_test = train_test_split(X,y,random_state =0)
 
-#makes new list of user IDs only if user_ID has five or more counts in dictionary
-for user_id, count in id_counts.items():
-    if count >= 5:
-        new_users.append(user_id)
+mean_X = np.mean(X_train, axis=0)
+std_X = np.std(X_train, axis=0)
+mean_y = np.mean(y)
+std_y = np.std(y)
+X_train_norm = (X_train - mean_X) / std_X
+X_test_norm = (X_test - mean_X) / std_X
+y_train_norm = (y_train - mean_y) / std_y
 
-#makes new list of lists based off of filtered User IDs
-for row in dataset_1:
-    if row[0] in new_users:
-        clean_data.append(row)
+# Train Ridge regression model
+alpha = 0.50  # Regularization strength
+ridge_model = Ridge(alpha=alpha)
+ridge_model.fit(X_train_norm, y_train)
 
-print('Length of data set after 5 video filter: ', len(clean_data))
+# Predict on test set
+y_pred_test = ridge_model.predict(X_test_norm)
 
-#converts to array
-X = np.array(clean_data)
-
-#Example of filtered data 
-print('First two rows of cleaned data: ', X[:2])
-
-#As you can see from the results of this line of code ^^^ all the values are strings, 
-#im not sure how to convert each one into a float/integer without changing the user ID into a float as well (results in an error)
-
-
-
-
-
-def makePointList(data):
-    """Creates a list of points from initialization data.
-    #This function is outside Point Class.
-    Args:
-      data: A p-by-d numpy array.
-
-    Returns:
-      A list of length p containing d-dimensional Point objects, each Point's
-      coordinates correspond to one row of data.
-    """
-    list = []
-    # fill in
-    for x in data:
-        a = Point(x.tolist())
-        list.append(a)
-
-
-    return list
-
-#train data
-
-#validate data
-
-#test data
-
-
-#Main function
-#if __name__ == "__main__":
-
-    points = makePointList(clean_data)
-    print(points[0])
+# Evaluate model
+mse = mean_squared_error(y_test, y_pred_test)
+r2 = r2_score(y_test, y_pred_test)
+print('Mean squared error with normalization:', mse)
+print('Coefficient of Determination:', r2)
